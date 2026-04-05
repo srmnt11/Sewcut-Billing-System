32cb8a3a2fe21d1ebc1d97203efe02eb7c67a022
Samuel <qsjjsarmiento@tip.edu.ph>
2026-04-05 17:18:20 +0800

    Prevent email endpoint worker timeouts on Render

--- sewcut-backend/sewcut/settings.py
+++ sewcut-backend/sewcut/settings.py
@@ -271,6 +271,7 @@ EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.smtp.
 EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = env_bool('EMAIL_USE_TLS', True)
+EMAIL_TIMEOUT = int(os.environ.get('EMAIL_TIMEOUT', 20))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
